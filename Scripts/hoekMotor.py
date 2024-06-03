817fee74ca91b30a5a5c1d366c136e269fcbee1b
Yornan04 <yorandevries2004@gmail.com>
2024-06-03 13:30:50 +0200

    motor verbetering

--- Scripts/hoekMotor.py
+++ Scripts/hoekMotor.py
@@ -3,7 +3,19 @@ import smbus
 import time
 import numpy as np
 #from nep import motor
-nepMotor = False
+maxSpeeds = {
+    45:400,
+    30:350,
+    15:200
+}
+
+def getMaxSpeed(angle):
+    nearest_angle = None
+    for threshold_angle in sorted(maxSpeeds.keys()):
+        if angle <= threshold_angle:
+            nearest_angle = threshold_angle
+            break
+    return maxSpeeds[nearest_angle]
 
 def ReadRawAngle(): # Read angle (0-360 represented as 0-4096)
   read_bytes = bus.read_i2c_block_data(DEVICE_AS5600, 0x0C, 2)
@@ -15,106 +27,43 @@ def ReadAngle(offset = 0):
 try:
     dynamixel_id = 21
     maxSpeed = 500
-    if not nepMotor:
-        DEVICE_AS5600 = 0x36 # Default device I2C address
-        bus = smbus.SMBus(3)
-        
-        serial_connection = Connection(port="/dev/serial0", baudrate=57600, rpi_gpio=True)
-        serial_connection.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
-        serial_connection.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
-        startAngle = ReadAngle(5.5)
-        endAngle = int(input("hoek: "))
-        totalAngle = abs(endAngle-startAngle)
-        direction = int(endAngle-startAngle<0)*0x400
-        timeSteps = 20
-        angleLeft = totalAngle
-        currentSpeed = 0
-        for x in range(timeSteps):
-            if(angleLeft<=totalAngle*2/3):
-                break
-            currentSpeed = maxSpeed/timeSteps*(x+1)
-            serial_connection.set_speed(dynamixel_id,direction+int(currentSpeed))
-            a = ReadAngle(5.5)
-            angleLeft = abs(endAngle - a)
-            print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
-            time.sleep(0.05)
-        print()
-
-        #continue when not halfway
-        while (angleLeft>=totalAngle/2):
-            a = ReadAngle(5.5)
-            angleLeft = abs(endAngle - a)
-            if(angleLeft<30):
-                serial_connection.set_speed(dynamixel_id,direction+300)
-                currentspeed = 300
-                break
-            print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
-            time.sleep(0.05)
-        midAngle = angleLeft
-        midSpeed = currentSpeed
-        print()
-
-        #decelerate when halfway
-        while abs(angleLeft)>2:
-            a = ReadAngle(5.5)
-            angleLeft = endAngle - a
-            speed=midSpeed*angleLeft/midAngle
-            speed=max(abs(speed),300)
-            currentSpeed = speed
-            if(abs(currentSpeed)>maxSpeed): break
-            print(speed)
-            speed = direction+speed
-            serial_connection.set_speed(dynamixel_id,int(speed))
-            print(f"angle: {a}, speed: {speed}, angleLeft: {angleLeft}")
-            time.sleep(0.05)
-        serial_connection.set_speed(dynamixel_id,0)
+    minSpeed = 200
+    DEVICE_AS5600 = 0x36 # Default device I2C address
+    bus = smbus.SMBus(3)
+    serial_connection = Connection(port="/dev/serial0", baudrate=57600, rpi_gpio=True)
+    serial_connection.set_cw_angle_limit(dynamixel_id, 0, degrees=False)
+    serial_connection.set_ccw_angle_limit(dynamixel_id, 0, degrees=False)
+    startAngle = ReadAngle(5.5)
+    endAngle = int(input("hoek: "))
+    totalAngle = abs(endAngle-startAngle)
+    direction = int(endAngle-startAngle<0)*0x400
+    timeSteps = 9
+    angleLeft = totalAngle
+    currentSpeed = 0
+    
+    for x in range(timeSteps):
+        currentSpeed = minSpeed-100+(100+maxSpeed-minSpeed)*x/(timeSteps-1)
+        serial_connection.set_speed(dynamixel_id,direction+currentSpeed)
         a = ReadAngle(5.5)
+        angleLeft = abs(endAngle - a)
+        if angleLeft<max(maxSpeeds.keys()) and getMaxSpeed(angleLeft)<=currentSpeed:
+            break
         print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
-
-    else:
-        m = motor(id=1, maxSpeed=500, minSpeed=200, startAngle=90)
-        endAngle = -90
-        totalAngle = abs(endAngle-m.angle)
-        direction = np.sign(endAngle-m.angle)
-        timeSteps = 10
-        angleLeft = totalAngle
-        currentSpeed = 0
-        for x in range(timeSteps):
-            if(angleLeft<=totalAngle/2):
-                break
-            currentSpeed = m.maxSpeed/timeSteps*(x+1)
-            m.set_speed(dynamixel_id,direction*currentSpeed)
-            a = m.ReadAngle()
-            angleLeft = abs(endAngle - a)
-            print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
-            time.sleep(0.1)
-        print()
-
-        #continue when not halfway
-        while (angleLeft>=totalAngle/2):
-            
-            a = m.ReadAngle()
-            angleLeft = abs(endAngle - a)
-            print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
-            time.sleep(0.1)
-
-        midAngle = angleLeft
-        midSpeed = currentSpeed
-        print()
-
-        #decelerate when halfway
-        while abs(angleLeft)>1:
-            a = m.ReadAngle()
-            angleLeft = endAngle - a
-            speed=midSpeed*angleLeft/midAngle
-            speed=int(np.sign(speed)*max(abs(speed),200))
-            currentSpeed = speed
-            if(abs(currentSpeed)>maxSpeed): break
-            m.set_speed(dynamixel_id,speed)
-            print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
-            time.sleep(0.1)
-        m.set_speed(dynamixel_id,0)
-        print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
+        time.sleep(0.1)
+    print()
+    while angleLeft>max(maxSpeeds.keys()):
+        a = ReadAngle(5.5)
+        angleLeft = abs(endAngle - a)
+        print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
+        time.sleep(0.1)
+    while abs(angleLeft)>2:
+        a = ReadAngle(5.5)
+        angleLeft = abs(endAngle - a)
+        currentSpeed = getMaxSpeed(angleLeft)
+        serial_connection.set_speed(dynamixel_id,direction+getMaxSpeed(angleLeft))
+        print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
+        time.sleep(0.05)
+    print(f"angle: {a}, speed: {currentSpeed}, angleLeft: {angleLeft}")
             
 except Exception as e:
     print(f"Error: {e}")
--- Scripts/hoekMotorFake.py
+++ Scripts/hoekMotorFake.py
@@ -7,27 +7,47 @@ maxSpeeds = {
     30:350,
     15:250
 }
+
+def getMaxSpeed(angle):
+    nearest_angle = None
+    for threshold_angle in sorted(maxSpeeds.keys()):
+        if angle <= threshold_angle:
+            nearest_angle = threshold_angle
+            break
+    return maxSpeeds[nearest_angle]
 try:
     dynamixel_id = 21
     maxSpeed = 500
-    m = motor(id=1, maxSpeed=500, minSpeed=200, startAngle=45)
-    endAngle = 0
+    m = motor(id=1, maxSpeed=500, minSpeed=200, startAngle=-15)
+    endAngle = 15
     totalAngle = abs(endAngle-m.angle)
     direction = np.sign(endAngle-m.angle)
-    timeSteps = 10
+    timeSteps = 9
     angleLeft = totalAngle
     currentSpeed = 0
-    print(maxSpeeds[max(maxSpeeds)])
+    
     for x in range(timeSteps):
-        if(angleLeft<=totalAngle/2):
+        currentSpeed = m.minSpeed-100+(100+m.maxSpeed-m.minSpeed)*x/(timeSteps-1)
+        m.set_speed(dynamixel_id,direction*currentSpeed)
+        a = m.ReadAngle()
+        angleLeft = abs(endAngle - a)
+        if angleLeft<max(maxSpeeds.keys()) and getMaxSpeed(angleLeft)<=currentSpeed:
             break
-        currentSpeed = m.minspeed-100+(100+m.maxspeed-m.minspeed)*x/(timeSteps-1)
-        m.setSpeed()
+        print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
+        time.sleep(0.1)
+    print()
+    while angleLeft>max(maxSpeeds.keys()):
         a = m.ReadAngle()
         angleLeft = abs(endAngle - a)
         print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
         time.sleep(0.1)
-    print()
+    while abs(angleLeft)>2:
+        a = m.angle
+        angleLeft = abs(endAngle - a)
+        m.set_speed(dynamixel_id,direction*getMaxSpeed(angleLeft))
+        m.ReadAngle()
+        print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
+        time.sleep(0.1)
     print(f"angle: {a}, speed: {m.speed}, angleLeft: {angleLeft}")
             
 except Exception as e:
